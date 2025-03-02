24655f12fbbaad1262887d278752a46a4245326b
kotta-27 <al20013@shibaura-it.ac.jp>
2025-03-03 03:36:37 +0900

    [add]worksの整備、devicon設定

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -18,6 +18,24 @@ const Works = () => {
     projectData = projectDataEn;
   }
 
+  // 言語に応じたアイコンのマッピング
+  const iconMapping = {
+    react: ["devicon-react-original colored", "#61DAFB"],
+    nextjs: ["devicon-nextjs-plain colored", "#000000"],
+    tailwind: ["devicon-tailwindcss-plain colored", "#38BDF8"],
+    python: ["devicon-python-plain colored", "#306998"],
+    js: ["devicon-javascript-plain colored", "#F0DB4F"],
+    java: ["devicon-java-plain colored", "#007396"],
+    unity: ["devicon-unity-plain colored", "#000000"],
+    c: ["devicon-c-plain colored", "#555555"],
+    cpp: ["devicon-cplusplus-plain colored", "#00599C"],
+    csharp: ["devicon-csharp-plain colored", "#239120"],
+    django: ["devicon-django-plain colored", "#092E20"],
+    html: ["devicon-html5-plain colored", "#E34F26"],
+    css: ["devicon-css3-plain colored", "#1572B6"],
+    postgres: ["devicon-postgresql-plain colored", "#336791"],
+  };
+
   return (
     <div className="py-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -38,17 +56,18 @@ const Works = () => {
                 </div>
                 <div className="px-4 py-2">
                   <div className="flex items-center bg-gray-100 rounded border border-gray-400">
-                    <i className="fa-regular fa-folder-open text-gray-500 py-2 px-2"></i>
-                    {project.langs.map((lang) => (
-                      <i
-                        key={lang}
-                        className={`fa-brands fa-${lang} text-3xl py-2 px-1 text-gray-800 ml-6`}
-                      ></i>
-                    ))}
-                    {/* GitHub link with Tailwind styles */}
-                    <a href={project.gitHubLink} className="ml-auto">
-                      <i className="fa-brands fa-github text-3xl py-2 px-2 text-gray-700 hover:text-gray-300 transition-colors duration-300"></i>
-                    </a>
+                    <div className="flex mx-auto">
+                      {project.langs.map((lang) => (
+                        <i
+                          key={lang}
+                          className={`${iconMapping[lang]?.[0]} text-2xl py-2 px-1 ml-2 mr-2`}
+                          style={{ color: iconMapping[lang]?.[1] }}
+                        ></i>
+                      ))}
+                      <a href={project.gitHubLink} className="ml-auto" target="_blank">
+                        {/* <i className="devicon-github-original text-xl py-2 px-2 text-gray-700 hover:text-gray-300 transition-colors duration-300"></i> */}
+                      </a>
+                    </div>
                   </div>
                 </div>
                 <div className="p-4 flex-grow">
--- src/components/data.js
+++ src/components/data.js
@@ -5,7 +5,7 @@ export const projectDataJa = [
     id: 1,
     title: "Todo List App",
     image: "/todoapp2.png",
-    langs: ["react"],
+    langs: ["react", "nextjs", "tailwind"],
     description:
       "シンプルなTO-DOアプリで，next.jsを用いて作成．jsonのサーバはローカル上で動作するライブラリであるjson-serverを用いて，モダンCSSフレームワークであるtailwindも使用．",
     gitHubLink: "https://github.com/olawanlejoel/Todo-List-App",
@@ -14,7 +14,7 @@ export const projectDataJa = [
     id: 2,
     title: "Shift Management App",
     image: "/shiftapp.png",
-    langs: ["python", "js"],
+    langs: ["django", "python", "js", "html", "css"],
     description:
       "高度情報演習1Bの授業の開発として作成．PythonのWebアプリケーションフレームワークであるdjangoを用いてシフト管理アプリを作成．カレンダーはJavaScriptで制御し，従業員同士のチャット機能も実装．",
     gitHubLink: "https://github.com/olawanlejoel/Book-Library",
@@ -33,7 +33,7 @@ export const projectDataJa = [
     id: 4,
     title: "Assignment Management App",
     image: "/assapp.png",
-    langs: ["java"],
+    langs: ["java", "postgres"],
 
     description:
       "高度情報演習2Cの開発として作成．JavaのGUIフレームワークであるJavaFXを用いて作成し，JDBCを用いてPostgresデータベースと接続．時刻によるリマインダも実装．",
@@ -43,7 +43,7 @@ export const projectDataJa = [
     id: 5,
     title: "Block-Devided Pointing App",
     image: "/pointapp.png",
-    langs: ["unity"],
+    langs: ["unity", "c#"],
 
     description:
       "高度情報演習2Cの開発として作成．Unity(C#)による作成．ブロック分割によるポインティングアプリで，従来法と統計的に比較を行った．投票で最多票を獲得．",
@@ -53,7 +53,7 @@ export const projectDataJa = [
     id: 6,
     title: "Bingo App",
     image: "/bingoapp.png",
-    langs: ["react"],
+    langs: ["c"],
 
     description:
       "文化祭のシミュレートの一環で作成．5x5のビンゴカードを用いた時，統計的に平均何回でビンゴになるかを検証するために作成．C言語による実装．",
@@ -66,7 +66,7 @@ export const projectDataEn = [
     id: 1,
     title: "Todo List App",
     image: "/todoapp2.png",
-    langs: ["react"],
+    langs: ["react", "nextjs", "tailwind"],
     description:
       "Simple TO-DO app created using next.js. The json server is a local library that runs on the local server, and the modern CSS framework tailwind is also used. ",
     gitHubLink: "https://github.com/olawanlejoel/Todo-List-App",
@@ -75,7 +75,7 @@ export const projectDataEn = [
     id: 2,
     title: "Shift Management App",
     image: "/shiftapp.png",
-    langs: ["python", "js"],
+    langs: ["django", "python", "js", "html", "css"],
     description:
       "Developed as part of the first year of the Advanced Information Seminar. Created a shift management app using Django, a Python web application framework. The calendar is controlled using JavaScript, and the chat function between employees is also implemented. ",
     gitHubLink: "https://github.com/olawanlejoel/Book-Library",
@@ -93,7 +93,7 @@ export const projectDataEn = [
     id: 4,
     title: "Assignment Management App",
     image: "/assapp.png",
-    langs: ["java"],
+    langs: ["java", "postgres"],
     description:
       "Developed as part of the second year of the Advanced Information Seminar. Created an assignment management app using JavaFX, a Java GUI framework. The app connects to a Postgres database using JDBC, and a reminder function is also implemented based on time. ",
     gitHubLink: "https://github.com/olawanlejoel/Password-Generator",
@@ -102,7 +102,7 @@ export const projectDataEn = [
     id: 5,
     title: "Block-Devided Pointing App",
     image: "/pointapp.png",
-    langs: ["unity"],
+    langs: ["unity", "csharp"],
     description:
       "Developed as part of the second year of the Advanced Information Seminar. Created a pointing app using Unity (C#). The app is a block-devided pointing game, and the traditional method and statistical comparison were conducted. It received the most votes in the voting. ",
     gitHubLink: "https://github.com/olawanlejoel/TwitterUI-clone",
@@ -111,7 +111,7 @@ export const projectDataEn = [
     id: 6,
     title: "Bingo App",
     image: "/bingoapp.png",
-    langs: ["react"],
+    langs: ["c"],
     description:
       "Created as part of the cultural festival. Created a bingo card app for 5x5 bingo cards to verify statistically how many times it takes to get bingo. Implemented in C. ",
     gitHubLink: "https://github.com/olawanlejoel/TwitterUI-clone",
