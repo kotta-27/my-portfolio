8af34c1fea8475f504351655075db0e3a1a0a995
kotta-27 <al20013@shibaura-it.ac.jp>
2025-03-03 04:20:52 +0900

    [fix]navbarレスポンシブ

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,47 +44,48 @@ const Navbar = () => {
   };
 
   return (
-    <nav className={`fixed top-0 left-0 w-full bg-zinc-800 z-50 border-b border-zinc-700 transition-all
-      duration-300`}>
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 w-3/3 md:w-5/6">
-        <div className="relative flex h-16 items-center justify-between">
-          <div className="flex flex-1 items-center justify-between sm:items-stretch sm:justify-start">
-            <div className="flex items-center">
-              {/* logo */}
-              <a
-                href="/"
-                className={`text-3xl font-bold text-white `}
-                onClick={scrollToTop}
-              >
-                Kota's Portfolio
-              </a>
-            </div>
+    <nav className="fixed top-0 left-0 w-full bg-zinc-800 z-50 border-b border-zinc-700 transition-all duration-300">
+      <div className="flex h-16 items-center justify-between">
+        {/* logo */}
+        <div className="w-full sm:w-1/3 flex items-center">
+          <a
+            href="/"
+            className="text-3xl font-bold text-white lg:pl-20 pl-5"
+            onClick={scrollToTop}
+          >
+            Kota's Portfolio
+          </a>
+        </div>
 
-            {/* メニュー */}
-            <div className="ml-10 border-l border-gray-600 w-5" />
-            <div className="hidden sm:ml-6 sm:block">
-              <div className="flex space-x-4 justify-center items-center">
-                {[
-                  { id: 'about', text: 'About' },
-                  { id: 'skills', text: 'Skills' },
-                  { id: 'applications', text: 'Applications' },
-                  { id: 'works', text: 'Works' }
-                ].map((item) => (
-                  <a
-                    key={item.id}
-                    href={`#${item.id}-name`}
-                    className="rounded px-3 py-2 text-base font-bold text-gray-300 hover:bg-gray-300 hover:text-black transition duration-150"
-                    onClick={(e) => scrollToSection(e, `#${item.id}-name`)}
-                  >
-                    {item.text}
-                  </a>
-                ))}
-              </div>
-            </div>
+        {/* 区切り線 */}
+        {/* <div className="ml-10 border-l border-gray-600 w-5" /> */}
 
-            {/* もしjaなら英語への切り替えボタン、enなら日本語への切り替えボタン */}
-            {/* モバイルの場合は非表示 */}
-            <div className="absolute sm:right-0 hidden sm:block">
+        {/* メニュー */}
+        <div className="flex flex-row sm:w-2/3 border-l border-gray-600">
+          <div className="hidden sm:ml-6 sm:block w-2/3">
+            <div className="flex sm:space-x-4 space-x-2 items-center">
+              {[
+                { id: 'about', text: 'About' },
+                { id: 'skills', text: 'Skills' },
+                { id: 'applications', text: 'Applications' },
+                { id: 'works', text: 'Works' }
+              ].map((item) => (
+                <a
+                  key={item.id}
+                  href={`#${item.id}-name`}
+                  className="rounded py-2 text-base font-bold text-gray-300 hover:bg-gray-300 hover:text-black transition duration-150"
+                  onClick={(e) => scrollToSection(e, `#${item.id}-name`)}
+                >
+                  {item.text}
+                </a>
+              ))}
+            </div>
+          </div>
+
+          {/* もしjaなら英語への切り替えボタン、enなら日本語への切り替えボタン */}
+          {/* モバイルの場合は非表示 */}
+          <div className="hidden sm:block w-1/3">
+            <div className="flex justify-end md:pr-20 pr-5">
               <button
                 onClick={() => setLanguage(language === "ja" ? "en" : "ja")}
                 className="px-0 sm:px-4 py-2 w-10 sm:w-24 text-center border transition-colors rounded-md bg-gray-700 text-white"
@@ -92,55 +93,57 @@ const Navbar = () => {
                 {language === "ja" ? "English" : "日本語"}
               </button>
             </div>
+          </div>
 
-          </div>
-          <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
-            {/* Mobile menu button */}
-            <button
-              onClick={() => setIsOpen(!isOpen)}
-              type="button"
-              className="inline-flex items-center justify-center rounded-md p-2 text-zinc-400 hover:bg-zinc-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-              aria-controls="mobile-menu"
-              aria-expanded="false"
-            >
-              <span className="sr-only">Open main menu</span>
-              {!isOpen ? (
-                <svg
-                  className="h-6 w-6 block"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  className="h-6 w-6 block"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              )}
-            </button>
-          </div>
         </div>
       </div>
+
+      <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
+        {/* Mobile menu button */}
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          type="button"
+          className="inline-flex items-center justify-center rounded-md p-2 text-zinc-400 hover:bg-zinc-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+          aria-controls="mobile-menu"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          {!isOpen ? (
+            <svg
+              className="h-6 w-6 block"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          ) : (
+            <svg
+              className="h-6 w-6 block"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          )}
+        </button>
+      </div>
+
 
       {/* Mobile menu, show/hide based on menu state. */}
       {isOpen && (
